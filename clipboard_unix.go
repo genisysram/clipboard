7c45b8673834045ddc6dd553fcb65f3bf4224119
Zachary Yedidia <zyedidia@gmail.com>
2020-04-20 23:10:10 -0400

    Validate xsel and xclip before relying on them
    Ref zyedidia/micro#1557

--- clipboard_unix.go
+++ clipboard_unix.go
@@ -82,14 +82,18 @@ func init() {
 	copyCmdArgs = xclipCopyArgs
 
 	if _, err := exec.LookPath(xclip); err == nil {
-		return
+		if err := exec.Command("xclip", "-o").Run(); err == nil {
+			return
+		}
 	}
 
 	pasteCmdArgs = xselPasteArgs
 	copyCmdArgs = xselCopyArgs
 
 	if _, err := exec.LookPath(xsel); err == nil {
-		return
+		if err := exec.Command("xsel").Run(); err == nil {
+			return
+		}
 	}
 
 	pasteCmdArgs = termuxPasteArgs
